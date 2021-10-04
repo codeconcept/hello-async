2abb3b7537d129a1cb3881d77c4f1d1d6316a86b
codeconcept <contact@codeconcept.fr>
2021-10-04 13:01:06 +0200

    startDecisionProcess() function that returns a promise

--- index.js
+++ index.js
@@ -2,12 +2,30 @@ const btnJob = document.getElementById('btn-job');
 
 btnJob.addEventListener('click', applyToJob);
 
+const candidat = {
+  isProgrammer: true,
+  isCool: true,
+};
+
 function applyToJob() {
-  console.log('La décision est en cours...');
-  setTimeout(() => {
-    console.log('Vous avez le job');
-  }, 2000);
-  console.log("Pendant ce temps je reçois d'autres candidats");
+  console.log('Recruteur - La décision est en cours...');
+  const result = startDecisionProcess();
+  console.log('result', result);
+}
+
+function startDecisionProcess() {
+  return new Promise((resolve, reject) => {
+    console.log("Recruteur - Pendant ce temps je reçois d'autres candidats");
+    setTimeout(() => {
+      if (candidat.isProgrammer && candidat.isCool) {
+        resolve('Recruteur - Bienvenue dans notre entreprise !');
+      } else {
+        reject(
+          `Recruteur - Malgré tout l'intérêt de votre candidatur, nous somme au regret de bla bla`
+        );
+      }
+    }, 2000);
+  });
 }
 
 console.log('Après la fonction applyToJob');
