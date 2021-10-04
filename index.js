a5c04a28de49e8b8a4fbf834af010a23264ba153
codeconcept <contact@codeconcept.fr>
2021-10-04 12:48:19 +0200

    Simple applyToJob() function

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('coucou');
+const btnJob = document.getElementById('btn-job');
+
+btnJob.addEventListener('click', applyToJob);
+
+function applyToJob() {
+  console.log('La décision est en cours...');
+  setTimeout(() => {
+    console.log('Vous avez le job');
+  }, 2000);
+  console.log("Pendant ce temps je reçois d'autres candidats");
+}
+
+console.log('Après la fonction applyToJob');
